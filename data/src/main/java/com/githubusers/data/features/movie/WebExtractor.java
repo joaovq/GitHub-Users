683621d57af9a9f26ab6617bef7dd4ba11412a7f
joaovq <joaovq@gmail.com>
2018-01-24 20:02:18 -0200

    #feat: primeiro teste com extrator

--- data/src/main/java/com/githubusers/data/features/movie/WebExtractor.java
+++ data/src/main/java/com/githubusers/data/features/movie/WebExtractor.java
@@ -1,5 +1,14 @@
 package com.githubusers.data.features.movie;
 
+import android.util.Log;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 import io.reactivex.Observable;
 
 /**
@@ -13,6 +22,17 @@ public class WebExtractor {
         return Observable.create(emitter -> {
             MovieEntity movieEntity = new MovieEntity();
             movieEntity.setTitle("Teste");
+
+            try {
+                Document wikipedia = Jsoup.connect("https://pt.wikipedia.org/wiki/The_Shining").get();
+                Elements bodies = wikipedia.getElementsByTag("tbody");
+                Element infobox = bodies.get(0);
+                Elements colunas = infobox.getElementsByTag("tr");
+                Log.d(WebExtractor.class.getCanonicalName(), "getMovie: " + infobox);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
             emitter.onNext(movieEntity);
             emitter.onComplete();
